0327e1e3353a7a32a87bd20e94fba8c4d720ce03
daserge <daserge@yandex.ru>
2015-04-25 15:34:52 +0300

    Replaced workaround code for double click event issue

--- AADGraphClient/js/app.js
+++ AADGraphClient/js/app.js
@@ -4,19 +4,7 @@
 (function () {
     'use strict';
 
-  // TODO: Investigate why this code started to cause crashes on Windows 8.0/8.1
-  //   // This prevents event click to be called twice on Windows 8.1
-  //   window.addEventListener('click', function (event) {
-  //       if (typeof event.target !== 'undefined' && typeof event.target.attributes !== 'undefined'
-  //           && Array.prototype.slice.call(event.target.attributes, 0).filter(function (item) {
-  //               return item.nodeValue === 'checkbox';
-  //       }).length > 0) {
-  //           // This is a workaround for checkboxes not being checked in case of stopping propagation
-  //       } else if (Object.prototype.toString.call(event) == '[object PointerEvent]') {
-  //           event.stopPropagation();
-  //       }
-  //   }
-  // , true);
+    var elemsClickCount = {};    
 
     angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ngMessages'])
       .run(function ($ionicPlatform) {
@@ -26,9 +14,29 @@
               if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
                   cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
               }
+
               if (window.StatusBar) {
                   // org.apache.cordova.statusbar required
                   StatusBar.styleDefault();
+              }
+
+              if (window.cordova && window.cordova.platformId === 'windows') {
+                  // This prevents event click to be called twice on Windows 8.1
+                  window.addEventListener('click', function (event) {
+                      if (typeof event.target !== 'undefined' && typeof event.target.attributes !== 'undefined') {
+                          if (elemsClickCount[event.target.className] === 0
+                              || typeof elemsClickCount[event.target.className] === 'undefined') {
+                              elemsClickCount[event.target.className] = 1;
+                              return;
+                          } else {
+                              elemsClickCount[event.target.className] = 0;
+                              event.stopPropagation();
+                              elemsClickCount = {};
+                              return;
+                          }
+                      }
+                  }
+                , true);
               }
           });
       })
